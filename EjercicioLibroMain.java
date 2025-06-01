5267c612722ed2ff0ab0c433ed0223a873d93d1f
Your Name <you@example.com>
2025-06-01 17:57:09 -0600

    Se realizaron todos los codigos de los ejercicios

--- EjercicioLibroMain.java
+++ EjercicioLibroMain.java
@@ -2,13 +2,14 @@ import java.util.LinkedHashSet;
 
 public class EjercicioLibroMain {
     public static void main(String[] args) {
-        LinkedHashSet<EjercicioLibroMain> libros = new LinkedHashSet<>();
+        LinkedHashSet<Libro> libros = new LinkedHashSet<>();
+
         libros.add(new Libro("1", "El Alquimista", "1ra", "Paulo Coelho"));
         libros.add(new Libro("2", "Cien Años de Soledad", "1ra", "Gabriel García Márquez"));
         libros.add(new Libro("3", "Don Quijote", "2da", "Miguel de Cervantes"));
-        for (EjercicioLibroMain libro : libros) {
+
+        for (Libro libro : libros) {
             System.out.println(libro);
         }
     }
 }
-
